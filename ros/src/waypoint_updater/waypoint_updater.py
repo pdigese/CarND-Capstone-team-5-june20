f3e447733e4bb57e240d2b0c60c2a42f84660607
pdigese <philipp.digeser@gmx.de>
2020-06-08 10:12:46 -0700

    [ENHANCEMENT] Initial commit to test working with the repo. Started with waypoint updater node. Set throttle to const 1 to let the car accelerate in the simulator (works!).
    Added some code to the waypoint updater node, but its not finished yet. Trajectory is not built based on the given waypoints, but the waypoints are read.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from scipy.spatial import KDTree
 
 import math
 
@@ -38,22 +39,77 @@ class WaypointUpdater(object):
 
         # TODO: Add other member variables you need below
 
-        rospy.spin()
+        self.cyclic_traj_gen_and_publishing()
+
+    def cyclic_traj_gen_and_publishing(self):
+        '''
+        Does cyclic trajectory generation based on the current pose and the base trajectory
+        and publishes the planned trajectory.
+        f_cyclic = 30 Hz since more seems not to be required, since the provided information is
+        not updated more frequently.
+        '''
+        rate = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            # ENDLESS LOOP
+            wp_idx = self.get_closest_waypoint_idx()
+            self.publish_trajectory(wp_idx)
+            # "END" OF ENDLESS LOOP, wait the remaining time
+            rate.sleep()
+        pass
+
+    def get_closest_waypoint_idx(self):
+        '''
+        Get the waypoint index which is
+        - closest to the current waypoint (by KDTree query)
+        - in front of the car (by checking if the pointing end of the vector, which is being made from the 
+        current waypoint and the waypoint behind, is in front or behind the current car pose)
+        '''
+        nearest_wp_idx = self.waypoint_tree.query([
+            self.pose.pose.position.x,
+            self.pose.pose.position.y
+        ],1)[1]
+        closest_pt = np.array(self.waypoint_xy[nearest_wp_idx])
+        prev_pt = np.array(self.waypoint_xy[nearest_wp_idx-1]) # any chance of idx out of range?
+        curr_pt = np.array(self.pose.pose.position.x, self.pose.pose.position.y)
+        val = np.dot(closest_pt - prev_pt, curr_pt - closest_pt)
+        '''
+        TODO: Find your own approach to identify the closest point...
+        '''
+        if val > 0:
+            nearest_wp_idx = (nearest_wp_idx + 1) % len(nearest_wp_idx)
+        return nearest_wp_idx
+
+    def publish_trajectory(self, idx):
+        lane = Lane()
+        lane.header  = self.all_waypoints.header
+        lane.waypoints = self.all_waypoints.waypoints[idx:idx+LOOKAHEAD_WPS]    # what happens if idx becomes to large? will is start from the beginning on again?
+        self.final_waypoints_pub.publish(lane)
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        '''
+        Stores the current pose of the car in this instance
+        '''
+        self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        '''
+        Callback for the base way points, which are provided once after startup (To be verified!).
+        The waypoints contain pose and twist, however, we need the pose only.
+        '''
+        self.all_waypoints = waypoints
+        self.waypoint_xy = list()
+        if not self.waypoint_xy:
+            for waypoint in waypoints.waypoints:
+                self.waypoint_xy.append((waypoint.pose.pose.position.x, waypoint.pose.pose.position.y))
+            self.waypoint_tree = KDTree(self.waypoint_xy)
+            rospy.loginfo("All waypoints received!")
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
+        # TODO: Callback for /traffic_waypoint message. Shall be implemented once the tl detection works
         pass
 
     def obstacle_cb(self, msg):
-        # TODO: Callback for /obstacle_waypoint message. We will implement it later
+        # TODO: Callback for /obstacle_waypoint message. Shall be implemented once the obstacle detection works
         pass
 
     def get_waypoint_velocity(self, waypoint):
