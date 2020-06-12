6759b4fc04d774cc11febff2f1f9109c9ba9c638
pdigese <philipp.digeser@gmx.de>
2020-06-12 15:23:38 -0700

    [ENHANCEMENT, BUGFIX] Implementation of the improvement of the planner: It does not plan the velocity as well when a red traffic light appears. Bugfix in twist_controller
    The planner now takes the latest 80 trajectories (~52 meters) in front of the car to plan the velocity. Once a red traffic light's stopline is reported to the trajectory planner, it creates a linear decrease of the velocity from the point where the car is until the stopline (1 m ahead), where a velocity of 0 m/s is planned. Note that the planner somehow might not be able to stop if the traffic light switches to red when the car is less then 1 m ahead of the TL ("yellow light feature required"). The twist controller is buggy: Previously it was not able to brake (wrong parametrization). Braking works, but the controller uses brakes at constant speed to keep speed (to be optimized)

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -12,7 +12,7 @@ import cv2
 import yaml
 import math
 
-STATE_COUNT_THRESHOLD = 3
+STATE_COUNT_THRESHOLD = 2
 LOOK_AHEAD = 100
 
 class TLDetector(object):
@@ -37,11 +37,11 @@ class TLDetector(object):
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb,queue_size=1)
 
-	# List of positions that correspond to the line to stop in front of for a given intersection
+        # List of positions that correspond to the line to stop in front of for a given intersection
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
         self.stop_line_positions = self.config['stop_line_positions']
-	self.stop_wp_list = None
+        self.stop_wp_list = None
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
@@ -61,7 +61,7 @@ class TLDetector(object):
 
     def waypoints_cb(self, lane):
         self.waypoints = lane.waypoints
-	self.stop_wp_list = self.get_stop_line_wp(self.stop_line_positions)
+        self.stop_wp_list = self.get_stop_line_wp(self.stop_line_positions)
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -87,13 +87,20 @@ class TLDetector(object):
         if self.state != state:
             self.state_count = 0
             self.state = state
+            light_wp = -1
         elif self.state_count >= STATE_COUNT_THRESHOLD:
             self.last_state = self.state
             light_wp = light_wp if state == TrafficLight.RED else -1
             self.last_wp = light_wp
-            self.upcoming_red_light_pub.publish(Int32(light_wp))
         else:
-            self.upcoming_red_light_pub.publish(Int32(self.last_wp))
+            light_wp = -1
+        
+        # note: The video says to publish the stopline waypoint, however, the written instruction
+        # tells us to publish the index of the traffic light waypoint (ranging from 0...NUM_OF_TRAFFIC_LIGHTS-1)
+        # However, the planner is not interested in the index of the traffic line but in the index of the stop line
+        # where it is supposed to stop.
+        # gerr('published stopline idx: %d',light_wp)
+        self.upcoming_red_light_pub.publish(Int32(light_wp))
         self.state_count += 1
 
     def get_closest_waypoint(self, position):
@@ -108,27 +115,27 @@ class TLDetector(object):
         """
         #TODO implement
 
-	min_dist = float('inf')
-	index = None
-#	dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-	pos_x = position[0]
-	pos_y = position[1]
+        min_dist = float('inf')
+        index = None
+        #    dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        pos_x = position[0]
+        pos_y = position[1]
 
-	dl = lambda x1,y1,x2,y2 : math.sqrt((x1-x2)**2 + (y1-y2)**2 )
+        dl = lambda x1,y1,x2,y2 : math.sqrt((x1-x2)**2 + (y1-y2)**2 )
 
-	for i in range(len(self.waypoints)-1):
+        for i in range(len(self.waypoints)-1):
 
-	    dist = min(min_dist, dl(pos_x,pos_y,self.waypoints[i].pose.pose.position.x,self.waypoints[i].pose.pose.position.y))
-	    
-	    if dist < min_dist:
-		min_dist = dist
-		index = i
+            dist = min(min_dist, dl(pos_x,pos_y,self.waypoints[i].pose.pose.position.x,self.waypoints[i].pose.pose.position.y))
+            
+            if dist < min_dist:
+                min_dist = dist
+                index = i
 
-	return index
+        return index
 
 
 
-    def get_light_state(self):
+    def get_light_state(self, idx=None):
         """Determines the current color of the traffic light
 
         Args:
@@ -143,19 +150,23 @@ class TLDetector(object):
             return TrafficLight.UNKNOWN   ## originally False
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-	
+    
         #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        if idx == None:
+            return self.light_classifier.get_classification(cv_image)
+        else:
+            # TODO: Implementation which takes the current state if the indexed traffic light
+            return self.lights[idx].state
 
     def get_stop_line_wp(self,stop_line_positions):
-	
-	stop_line_index_list =[]
-		
-	for i in range(len(stop_line_positions)-1):
-	    stop_line_index = self.get_closest_waypoint(stop_line_positions[i])
-	    stop_line_index_list.append(stop_line_index)
+    
+        stop_line_index_list =[]
+            
+        for i in range(len(stop_line_positions)-1):
+            stop_line_index = self.get_closest_waypoint(stop_line_positions[i])
+            stop_line_index_list.append(stop_line_index)
 
-	return stop_line_index_list
+        return stop_line_index_list
 
 
     def process_traffic_lights(self):
@@ -176,35 +187,37 @@ class TLDetector(object):
 #            state = self.get_light_state(light)
 #            return light_wp, state
 
-	if self.pose and self.waypoints and self.stop_wp_list:
-#	    rospy.logerr('loop active')
-	    car_x = self.pose.pose.position.x
-	    car_y = self.pose.pose.position.y
-	    car_wp = self.get_closest_waypoint([car_x,car_y])	    
-	    
-	    min_delta_index = float('inf')
-	    closest_light_index = None
- 
-	    for i in range(len(self.stop_wp_list)-1):
-		delta_index = self.stop_wp_list[i]-car_wp
-	    	
-		if delta_index < 0:
-		    delta_index += len(self.waypoints) 	
-		
-		if delta_index < LOOK_AHEAD and delta_index < min_delta_index:
-		    min_delta_index = delta_index
-		    closest_light_index = self.stop_wp_list[i]
-	    rospy.logerr('car_wp:%d',car_wp)
-	     
-	    if closest_light_index:
-		rospy.logerr('closest_light_index:%d',closest_light_index)
-		cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-#		cv2.imwrite(("images/{}.jpg".format(float(rospy.get_time()))),cv_image)
-	
-		state = self.get_light_state()
-		rospy.logerr('light index:%d  state:%d',closest_light_index, state)
-		
-		return closest_light_index, state
+        if self.pose and self.waypoints and self.stop_wp_list:
+    #        rospy.logerr('loop active')
+            car_x = self.pose.pose.position.x
+            car_y = self.pose.pose.position.y
+            car_wp = self.get_closest_waypoint([car_x,car_y])        
+            
+            min_delta_index = float('inf')
+            closest_light_index = None
+            tl_idx = None
+    
+            for i in range(len(self.stop_wp_list)-1):
+                delta_index = self.stop_wp_list[i]-car_wp
+                    
+                if delta_index < 0:
+                    delta_index += len(self.waypoints)     
+                
+                if delta_index < LOOK_AHEAD and delta_index < min_delta_index:
+                    min_delta_index = delta_index
+                    closest_light_index = self.stop_wp_list[i]
+                    tl_idx = i
+            #rospy.logerr('car_wp:%d',car_wp)
+            
+            if closest_light_index:
+                #rospy.logerr('closest_light_index:%d',closest_light_index)
+                cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        #        cv2.imwrite(("images/{}.jpg".format(float(rospy.get_time()))),cv_image)
+            
+                state = self.get_light_state(tl_idx)   # FIXME: remove the argument to use camera tl detection!!!
+                #rospy.logerr('light index:%d  state:%d',closest_light_index, state)
+            
+                return closest_light_index, state
 
         return -1, TrafficLight.UNKNOWN
 
