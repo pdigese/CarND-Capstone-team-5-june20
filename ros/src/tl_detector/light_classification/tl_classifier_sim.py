a01547ae044b690da3e8f3de90d35b47a9dac2d4
Ruben Meul <meulruben@gmail.com>
2020-06-27 19:14:14 +0200

    fixes broken indentation

--- ros/src/tl_detector/light_classification/tl_classifier_sim.py
+++ ros/src/tl_detector/light_classification/tl_classifier_sim.py
@@ -15,14 +15,14 @@ class TLClassifierSim(object):
     def __init__(self):
         #TODO load classifier
 
-	weight = 'weights.epoch-06__loss-0.02387__.hdf5'
+		weight = 'weights.epoch-06__loss-0.02387__.hdf5'
 
-	weight_dir = 'model'
-	self.model = Model_tl()
+		weight_dir = 'model'
+		self.model = Model_tl()
 
-	self.model.load_weights(os.path.join(DIR,weight_dir,weight))
-    self.graph = tf.get_default_graph()
-	self.state = TrafficLight.UNKNOWN
+		self.model.load_weights(os.path.join(DIR,weight_dir,weight))
+		self.graph = tf.get_default_graph()
+		self.state = TrafficLight.UNKNOWN
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -36,17 +36,17 @@ class TLClassifierSim(object):
         """
         #TODO implement light color prediction
 	
-	tl_dict = {0:TrafficLight.RED , 1:TrafficLight.YELLOW ,2:TrafficLight.GREEN ,3:TrafficLight.UNKNOWN }
+		tl_dict = {0:TrafficLight.RED , 1:TrafficLight.YELLOW ,2:TrafficLight.GREEN ,3:TrafficLight.UNKNOWN }
 
-	image = cv2.resize(image,(100,100))
-	image = np.array([image])
-	with self.graph.as_default():
-	    output = (self.model.predict(image)).squeeze()
-	    y_pred = np.argmax(output)
-	    conf = output[y_pred] 
-	    rospy.logerr('confidence:%s',conf)
-	    if conf > 0.5:    	    
-			self.state = tl_dict[y_pred]
-			return self.state
+		image = cv2.resize(image,(100,100))
+		image = np.array([image])
+		with self.graph.as_default():
+			output = (self.model.predict(image)).squeeze()
+			y_pred = np.argmax(output)
+			conf = output[y_pred] 
+			rospy.logerr('confidence:%s',conf)
+			if conf > 0.5:    	    
+				self.state = tl_dict[y_pred]
+				return self.state
 
-        return TrafficLight.UNKNOWN
+			return TrafficLight.UNKNOWN
